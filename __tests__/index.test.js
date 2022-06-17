4b0f27f44e66bed6ca4c2452447647ebc8aaba4c
manylov <yellowmanylov@gmail.com>
2022-06-17 23:19:22 +0700

    refactor __tests__/index.test.js__

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,18 +3,22 @@ import YAML from 'js-yaml';
 import { getFixturePath, readFile } from './tests-utils.js';
 import { generateDifference } from '../src/index.js';
 
-test('trees difference (JSON)', () => {
-  const file1 = getFixturePath('json', 'hexlet-data-1.json');
-  const file2 = getFixturePath('json', 'hexlet-data-2.json');
-  const expected = readFile('expected', 'diff(hexlet-data).json');
+const EXPECTED = readFile('expected', 'diff(hexlet-data).json');
 
-  expect(generateDifference(file1, file2)).toEqual(JSON.parse(expected));
+test('trees difference (JSON)', () => {
+  const [file1, file2] = [
+    getFixturePath('json', 'hexlet-data-1.json'),
+    getFixturePath('json', 'hexlet-data-2.json'),
+  ];
+
+  expect(generateDifference(file1, file2)).toEqual(JSON.parse(EXPECTED));
 });
 
 test('trees difference (YAML)', () => {
-  const file1 = getFixturePath('yaml', 'hexlet-data-1.yml');
-  const file2 = getFixturePath('yaml', 'hexlet-data-2.yml');
-  const expected = readFile('expected', 'diff(hexlet-data).json');
+  const [file1, file2] = [
+    getFixturePath('yaml', 'hexlet-data-1.yaml'),
+    getFixturePath('json', 'hexlet-data-2.yaml'),
+  ];
 
-  expect(generateDifference(file1, file2)).toEqual(YAML.load(expected));
+  expect(generateDifference(file1, file2)).toEqual(YAML.load(EXPECTED));
 });
