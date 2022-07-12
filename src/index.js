b1a0f5798a56e5acdec72e3d248de8bd92b90fc0
manylov <yellowmanylov@gmail.com>
2022-07-12 15:40:36 +0700

    add default case in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import getParsedData from './utils.js';
 import generateDifference from './generateDifference.js';
 import format from './formatters/index.js';
 
-const gendiff = (filepath1, filepath2, formatOption) => {
+const gendiff = (filepath1, filepath2, formatOption = 'stylish') => {
   const data1 = getParsedData(filepath1);
   const data2 = getParsedData(filepath2);
   const diff = generateDifference(data1, data2);
